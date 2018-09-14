ac5506cd30b69cd288787409db72b30a5d3e80c5
folami1 <folysco@outlook.com>
2018-09-14 22:45:12 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <section class="container">
+    <div>
+      <vue-flux
+        :options="fluxOptions"
+        :images="fluxImages"
+        :transitions="fluxTransitions"
+        ref="slider">
+      </vue-flux>
+    </div>
+  </section>
+</template>
+
+<script>
+import { VueFlux, Transitions } from 'vue-flux';
+
+export default {
+  components: {
+      VueFlux
+   },
+ 
+   data: () => ({
+      fluxOptions: {
+         autoplay: true
+      },
+      fluxImages: [
+         'images/material.jpg',
+         'images/nigerian-schools-1024x483.jpg',
+         'images/bck1.jpeg'
+      ],
+      fluxTransitions: {
+         transitionBook: Transitions.transitionBook
+      }
+   })
+}
+</script>
+
+<style>
+
+</style>
+
---
+++ plugins/vue-flux.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import VueFlux from 'vue-flux'
+
+Vue.use(VueFlux)
